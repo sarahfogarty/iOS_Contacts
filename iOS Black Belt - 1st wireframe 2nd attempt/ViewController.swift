f37999123737c90ada4173380b6b5750caa7fb83
Sarah Fogarty <sarahefogarty@gmail.com>
2017-03-26 12:31:15 -0700

    Initial Commit

---
+++ iOS Black Belt - 1st wireframe 2nd attempt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Black Belt - 1st wireframe 2nd attempt
+//
+//  Created by Sarah Fogarty on 3/26/17.
+//  Copyright © 2017 Sarah Fogarty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
